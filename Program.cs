4a13480811d91895260d50e442e45a05ee7c87cb
eseegm97 <eseegm97@gmail.com>
2026-01-15 22:42:13 -0600

    code

--- Program.cs
+++ Program.cs
@@ -5,22 +5,16 @@ using Newtonsoft.Json;
 using System.Text.Json.Serialization;
 
 var currentDirectory = Directory.GetCurrentDirectory();
-
 var storesDirectory = Path.Combine(currentDirectory, "stores");
 
 var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
-Directory.CreateDirectory(salesTotalDir);
+Directory.CreateDirectory(salesTotalDir);   
 
 var salesFiles = FindFiles(storesDirectory);
 
 var salesTotal = CalculateSalesTotal(salesFiles);
 
-File.WriteAllText(Path.Combine(salesTotalDir, "totals.txt"), $"{salesTotal}{Environment.NewLine}");
-
-// foreach (var file in salesFiles)
-// {
-//     Console.WriteLine(file);
-// }
+File.AppendAllText(Path.Combine(salesTotalDir, "totals.txt"), $"{salesTotal}{Environment.NewLine}");
 
 IEnumerable<string> FindFiles(string folderName)
 {
@@ -31,7 +25,6 @@ IEnumerable<string> FindFiles(string folderName)
     foreach (var file in foundFiles)
     {
         var extension = Path.GetExtension(file);
-        
         if (extension == ".json")
         {
             salesFiles.Add(file);
@@ -44,16 +37,16 @@ IEnumerable<string> FindFiles(string folderName)
 double CalculateSalesTotal(IEnumerable<string> salesFiles)
 {
     double salesTotal = 0;
-
+    
     foreach (var file in salesFiles)
-    {
+    {      
         string salesJson = File.ReadAllText(file);
-
+    
         SalesData? data = JsonConvert.DeserializeObject<SalesData?>(salesJson);
-
+    
         salesTotal += data?.Total ?? 0;
     }
-
+    
     return salesTotal;
 }
 
